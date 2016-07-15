f56e19b74c0d10676a7572db2c47dd7e16d139b9
lion <fb02@yandex.ru>
2016-07-15 15:40:40 +0300

    Update

---
+++ src/com/JavaStudent/Shildt/part1/Chapter05/DoWhile.java
@@ -0,0 +1,12 @@
+package com.JavaStudent.Shildt.part1.Chapter05;
+
+public class DoWhile {
+    public static void main(String args[]) {
+        int n = 10;
+
+        do {
+            System.out.println("такт " + n);
+            n--;
+        } while(n > 0);
+    }
+}
