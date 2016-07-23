de86cf9020a6bb715b0305aa25f2bc0ab3622fa0
lion <fb02@yandex.ru>
2016-07-24 00:56:45 +0300

    update

---
+++ src/com/JavaStudent/Shildt/pert2/Chapter18/ArrayListToArray.java
@@ -0,0 +1,26 @@
+package com.JavaStudent.Shildt.pert2.Chapter18;
+
+import java.util.ArrayList;
+
+public class ArrayListToArray {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+
+        System.out.println("Содержимое списочного массива: " +al);
+
+        Integer[] ia = new Integer[al.size()];
+        ia = al.toArray(ia);
+
+        int sum = 0;
+
+        for (Integer i : ia) sum +=i;
+
+        System.out.println("Сумма: " +sum);
+    }
+
+}
